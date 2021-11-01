340fae13f5f311932027f9955bad7c44ac8fd2d0
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-01 17:23:21 -0400

    added services section

--- capture/src/pages/AboutUs.js
+++ capture/src/pages/AboutUs.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 //Page Components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs= () => {
+    // using <> is called a fraction which lets us put components on the page without the use of a separate div
     return(
+    <>
 <AboutSection/>
-    )
-}
+<ServicesSection/>
+    </>)
+};
 
 export default AboutUs;
