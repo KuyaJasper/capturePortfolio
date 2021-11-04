e526fbb24b55b7e2636e676b086ddba406fb2b4a
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-03 21:28:53 -0400

    set up useEffect to filter out user history with useHistory

--- capture/src/pages/MovieDetail.js
+++ capture/src/pages/MovieDetail.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import{MovieState} from '../movieState';
 
 const MovieDetail = () => {
+
+    const history = useHistory();
+    const url = history.location.pathname;
+    const [movies, setMovies] = useState(MovieState);
+        const [movie,setMovie]= useState(null);
+
+//usetEffect
+
+useEffect = (() => {
+  const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
+  setMovie(currentMovie);
+
+}, [movies,url]);
+
   return (
     <div>
       <h1>MovieDetail</h1>
