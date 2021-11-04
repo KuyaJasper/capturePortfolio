739a997e8cf673a17f81dc0317892a37a77aed92
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-03 20:04:19 -0400

    set up react router and switch in app.js

--- capture/src/App.js
+++ capture/src/App.js
@@ -4,16 +4,30 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 
 //Import Pages
-
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+//Router
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
+      <Switch>
+      <Route path="/" exact>
       <AboutUs />
+      </Route>
+      <Route path="/work">
+      <OurWork/>
+      </Route>
+      <Route path="/contact">
+      <ContactUs/>
+      </Route>
+      </Switch>
     </div>
   );
 }
