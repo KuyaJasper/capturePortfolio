f9fcb6fba0daa85a28a80fe8c3ace63df874052b
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-03 21:44:53 -0400

    started dynamic rendering for movie details

--- capture/src/pages/MovieDetail.js
+++ capture/src/pages/MovieDetail.js
@@ -1,28 +1,40 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
-import{MovieState} from '../movieState';
+import { MovieState } from "../movieState";
 
 const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
 
-    const history = useHistory();
-    const url = history.location.pathname;
-    const [movies, setMovies] = useState(MovieState);
-        const [movie,setMovie]= useState(null);
+  //usetEffect
 
-//usetEffect
-
-useEffect = (() => {
-  const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
-  setMovie(currentMovie);
-
-}, [movies,url]);
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie[0]);
+  }, [movies, url]);
 
   return (
-    <div>
-      <h1>MovieDetail</h1>
-    </div>
+    <>
+      {movie && (
+        <Details>
+          <Headline>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie image" />
+            
+          </Headline>
+        </Details>
+      )}
+    </>
   );
 };
+
+//styling
+
+const Details = styled.div``;
+
+const Headline = styled.div``;
 
 export default MovieDetail;
