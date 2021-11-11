817e2a2844137bf6c2d4fd79e87772c2e4b94de1
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-10 21:58:36 -0500

    changed framer motion version in package json. added framer motion to about section

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -1,36 +1,57 @@
-import React from 'react';
-import home1 from '../img/home1.png';
+import React from "react";
+import home1 from "../img/home1.png";
 
 //styled components
-import styled from 'styled-components';
-import { About, Description, Image, Hide } from '../styles';
+import { About, Description, Image, Hide } from "../styles";
 
-const AboutSection = () =>{
-    return (
-        <About>
-            <Description>
-                <div className="title">
-                    <Hide>
-                        <h2>We work to make</h2>
-                    </Hide>
-                    <Hide>
-                        <h2>your <span>dreams</span> come</h2>
-                    </Hide>
-                    <Hide>
-                        <h2>true</h2>
-                    </Hide>
-                </div>
-                <p>Contact us for any photography or videography ideas that you have!</p>
-                <button>Contact Us</button>
-            </Description>
-            <Image>
-                <img src={home1} alt="man with a camera." />
-            </Image>
-        </About>
-    );
+// framer motion
+import { motion } from "framer-motion";
+
+const AboutSection = () => {
+  const titleAnimation = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: { duration: 0.5, ease: "easeOut", staggerChildren: 1 },
+    },
+  };
+
+  return (
+    <About>
+      <Description>
+        <motion.div
+          variants={container}
+          className="title"
+          initial="hidden"
+          animate="show"
+        >
+          <Hide>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnimation}>
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnimation}>true</motion.h2>
+          </Hide>
+        </motion.div>
+        <p>
+          Contact us for any photography or videography ideas that you have!
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="man with a camera." />
+      </Image>
+    </About>
+  );
 };
-
-
-
 
 export default AboutSection;
