a3d357ddf9e3479dc78083be5b0959b8beba1318
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-10 22:04:16 -0500

    reset page animation

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -8,38 +8,36 @@ import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
 
 const AboutSection = () => {
-  const titleAnimation = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 2 } },
-  };
 
-  const container = {
-    hidden: { x: 100 },
-    show: {
-      x: 0,
-      transition: { duration: 0.5, ease: "easeOut", staggerChildren: 1 },
-    },
-  };
+// ORIGINAL PAGE ANIMATIONS FROM LESSION
+//   const titleAnimation = {
+//     hidden: { opacity: 0 },
+//     show: { opacity: 1, transition: { duration: 2 } },
+//   };
+
+//   const container = {
+//     hidden: { x: 100 },
+//     show: {
+//       x: 0,
+//       transition: { duration: 0.5, ease: "easeOut", staggerChildren: 1, when:'afterChildren' },
+//     },
+//   };
 
   return (
     <About>
       <Description>
         <motion.div
-          variants={container}
-          className="title"
-          initial="hidden"
-          animate="show"
         >
           <Hide>
-            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
+            <motion.h2 >We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnimation}>
+            <motion.h2 >
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnimation}>true</motion.h2>
+            <motion.h2 >true</motion.h2>
           </Hide>
         </motion.div>
         <p>
