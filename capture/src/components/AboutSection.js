0c53fc9ff94c91164c3da6df7ff7d912589b2fdd
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-11-11 13:17:18 -0500

    set up animations for about page on load

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -6,47 +6,33 @@ import { About, Description, Image, Hide } from "../styles";
 
 // framer motion
 import { motion } from "framer-motion";
+import { titleAnimation,fade, photoAnimation } from "../animation";
 
 const AboutSection = () => {
-
-// ORIGINAL PAGE ANIMATIONS FROM LESSION
-//   const titleAnimation = {
-//     hidden: { opacity: 0 },
-//     show: { opacity: 1, transition: { duration: 2 } },
-//   };
-
-//   const container = {
-//     hidden: { x: 100 },
-//     show: {
-//       x: 0,
-//       transition: { duration: 0.5, ease: "easeOut", staggerChildren: 1, when:'afterChildren' },
-//     },
-//   };
 
   return (
     <About>
       <Description>
-        <motion.div
-        >
+        <motion.div>
           <Hide>
-            <motion.h2 >We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation} >We work</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 >
-              your <span>dreams</span> come
+            <motion.h2 variants={titleAnimation}>
+            to make your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 >true</motion.h2>
+            <motion.h2 variants={titleAnimation}>come true</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have!
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="man with a camera." />
+        <motion.img variants ={photoAnimation} src={home1} alt="man with a camera." />
       </Image>
     </About>
   );
